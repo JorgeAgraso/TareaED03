752dda5bd4e882c5273e58c0ea66356462d938f3
JorgeAgraso <jagrasoreyes@gmail.com>
2025-05-08 16:21:44 +0100

    Ejercicio terminado

--- src/ReOrdenar.java
+++ src/ReOrdenar.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class ReOrdenar {
+
+    public static int ordenarDesc(final int num) {
+
+        //Your code
+        int resultado = 0;
+        String numCadena = Integer.toString(num);
+        String[] arrayCadena = numCadena.split("");
+        ArrayList<Integer> listNum = new ArrayList<Integer>() ;
+        for (String i : arrayCadena){
+            Integer j = Integer.parseInt(i);
+            listNum.add(j);
+        }
+        Collections.sort(listNum, Comparator.reverseOrder());
+        String cadenaOrdenada = "";
+        for (Integer i : listNum){
+            cadenaOrdenada += Integer.toString(i);
+        }
+        resultado = Integer.parseInt(cadenaOrdenada);
+        return resultado;
+
+    }
+
 }
--- src/ReOrdenarTest.java
+++ src/ReOrdenarTest.java
@@ -1,4 +1,14 @@
+import org.testng.annotations.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
-class ReOrdenarTest {
-  
+
+public class ReOrdenarTest {
+
+    @Test
+
+    public void test_01() {
+        assertEquals(54421, ReOrdenar.ordenarDesc(42145));
+        assertEquals(654321, ReOrdenar.ordenarDesc(145263));
+        assertEquals(987654321, ReOrdenar.ordenarDesc(123456789));
+    }
 }
